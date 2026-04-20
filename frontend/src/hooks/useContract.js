37b7738dd602762372329d966b59557c75aa951c
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-04-20 13:24:08 +0300

    Added ABI

--- frontend/src/hooks/useContract.js
+++ frontend/src/hooks/useContract.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
+import ABI from "../WishBoard.json";
 
 const CONTRACT_ADDRESS = "0x7334b5086377DD68946343ad6eb539330D6822aB";
 
@@ -16,7 +17,7 @@ export default function useContract() {
 
     const provider = new ethers.BrowserProvider(window.ethereum);
     const signer = await provider.getSigner();
-    const contract = new ethers.Contract(CONTRACT_ADDRESS, signer);
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer);
 
     setContract(contract);
     setAccount(await signer.getAddress());
