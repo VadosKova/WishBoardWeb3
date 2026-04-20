9d031ca3c9acf5a6de5f916e60b5745043793091
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-04-20 13:07:22 +0300

    Created hook for contract

---
+++ frontend/src/hooks/useContract.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+
+const CONTRACT_ADDRESS = "0x7334b5086377DD68946343ad6eb539330D6822aB";
+
+export default function useContract() {
+  const [contract, setContract] = useState(null);
+  const [account, setAccount] = useState("");
+
+  useEffect(() => {
+    connect();
+  }, []);
+
+  const connect = async () => {
+    if (!window.ethereum) return alert("Install MetaMask");
+
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, signer);
+
+    setContract(contract);
+    setAccount(await signer.getAddress());
+  };
+
+  return { contract, account };
+}
